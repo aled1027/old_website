2d02151e16f6c150b267ab9d1dd1d9be55d2f754
aled1027 <a.led1027@gmail.com>
2015-04-29 20:33:21 -0700

    maybe finished minions.py?

--- minions.py
+++ minions.py
@@ -1,8 +1,9 @@
 def go(x,y,z):
     """
-    x           start
-    y           end
-    z           num iterations
+    x           first digit
+    y           last digit
+    z           number of digits in phone number
+                note that z digits => z-1 movements
     deltas      is a list of all the possible knight moves
     counter     tracks how many possible ways we can get to a position
     next_iter   tracks what numbers we've been at in the current iteration
@@ -28,27 +29,44 @@ def go(x,y,z):
         0:(1,4),
         }
 
+    positions = pos_dict.values()
     deltas = [(a,b) for a in [-1,1,-2,2] for b in [-1,1,-2,2] if abs(a) != abs(b)]
-    counter = {p:0 for p in pos_dict.values()}
+    prev_counter = {p : 0 for p in positions}
+
+
+    if z == 1:
+        return 1 if x==y else 0
 
     # apply first iteration
     x_pos = pos_dict[x]
-    prev_iter = [tuple_add(x_pos,d) for d in deltas if tuple_add(x_pos,d) in pos_dict.values()]
-    for p in prev_iter:
-        counter[p] += 1
+    x_plus_deltas = [tuple_add(x_pos,d) for d in deltas if tuple_add(x_pos,d) in positions]
+    for a in x_plus_deltas:
+        prev_counter[a] += 1
+    if z == 2:
+        return prev_counter[pos_dict[y]]
 
-    # do the rest of the iterations
-    for i in range(z-1):
-        prev_iter = next_iter if i!=0 else prev_iter
-        next_iter = []
-        for p in prev_iter:
-            for d in deltas:
-                cur_pos = tuple_add(p,d)
-                if cur_pos in pos_dict.values():
-                    next_iter.append(cur_pos)
-                    counter[cur_pos] += 1
+    """
+    - apply next z-2 iterations
+        - (meaning a total of z-1 iterations. see above for why z-1 iters)
+    - prev_counter is the counter for the previous iteration
+        - key is a position
+        - value is the number of ways we could have gotten to v
+    - cur_counter is the counter for the current iteration
+        - same format as prev_counter
+    """
 
-    return new[pos_dict[y]]
+    cur_counter = {}
+    for i in range(z-2):
+        prev_counter = cur_counter.copy() if i != 0 else prev_counter
+        cur_counter = {p : 0 for p in positions}
 
-print (go(1,1,2))
+        for p in prev_counter:
+            if prev_counter[p] == 0:
+                continue
+            p_plus_deltas = [tuple_add(p,d) for d in deltas if tuple_add(p,d) in positions]
+            for pos in p_plus_deltas:
+                cur_counter[pos] += prev_counter[p]
 
+    return cur_counter[pos_dict[y]]
+
+print (go(1,1,1))
